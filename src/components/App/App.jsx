a0aaf2530b9f8316e88aa45ed029375fa0218481
davydenko919 <davydenko919@gmail.com>
2024-08-21 00:40:35 +0300

    S1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,9 @@
-import Profile from '../Profile/Profile.jsx';
-import userData from "../../userData.json";
-import FriendList from '../FriendList/FriendList.jsx';
-import friends from "../../friends.json";
-import TransactionHistory from '../TransactionHistory/TransactionHistory.jsx';
-import transactions from "../../transactions.json";
+import css from "./App.module.css";,.;/
 
 export default function App() {
   return (
     <>
-      <Profile 
-        username={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        avatar={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} /> 
-      <TransactionHistory items={transactions} />
+      
     </>
   );
 }
